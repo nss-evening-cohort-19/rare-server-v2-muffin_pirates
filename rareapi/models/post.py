cb47fafecebf4d3d619af84435450cc116ea0044
Shalane Proctor <shalane088@gmail.com>
2022-12-13 19:16:19 -0600

    updated field on imageurl on post model

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     publication_date = models.DateTimeField(auto_now=True)
-    image_url = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200)
     content = models.CharField(max_length=1000)
     objects = models.Manager()
 
