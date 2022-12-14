f3b2a63a8e60b13c172b41af622119577c8d7b2c
Kristen Chandler <krismchandler@gmail.com>
2022-12-13 19:11:55 -0600

    updated created_on

--- rareapi/models/user.py
+++ rareapi/models/user.py
@@ -6,7 +6,7 @@ class User(models.Model):
     bio = models.CharField(max_length=1000)
     profile_image_url = models.URLField(max_length=200)
     email = models.EmailField(max_length=254)
-    created_on = models.DateField(default=date.today)
+    created_on = models.DateTimeField(auto_now=True)
     active = models.BooleanField()
     is_staff = models.BooleanField()
 
