27d3b3dbed71aee266ac98249fbc5a7928e34a83
Jeremiah King <usaxray25@gmail.com>
2022-12-13 19:30:57 -0600

    added post to commit

--- rareapi/models/category.py
+++ rareapi/models/category.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class Categories(models.Model):
+class Category(models.Model):
 
     label = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
