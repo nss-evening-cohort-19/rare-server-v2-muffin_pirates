fb1115e4b2e1cd5f302c610ca6bc5b781b7c4740
Kristen Chandler <krismchandler@gmail.com>
2022-12-13 19:09:48 -0600

    fixed spelling error

--- rareapi/models/user.py
+++ rareapi/models/user.py
@@ -4,7 +4,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     models.CharField(max_length=50)
     bio = models.CharField(max_length=1000)
-    profule_image_url = models.URLField(max_length=200)
+    profile_image_url = models.URLField(max_length=200)
     email = models.EmailField(max_length=254)
     created_on = models.DateField(default=date.today)
     active = models.BooleanField()
