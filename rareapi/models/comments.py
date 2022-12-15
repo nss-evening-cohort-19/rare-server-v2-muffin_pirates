5d6d8f76740a45e83b7f380d34ebb7874a0bfd00
Kristen Chandler <krismchandler@gmail.com>
2022-12-14 19:03:03 -0600

    fixed migration issues

--- rareapi/models/comments.py
+++ rareapi/models/comments.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from .user import User
 from .post import Post
@@ -7,4 +8,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    created_on = models.DateField(auto_now=False, auto_now_add=False)
+    created_on = models.DateField(("Date"), default=datetime.date.today)
+
+    def __str__(self):
+        return self.name
