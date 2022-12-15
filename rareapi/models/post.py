b0ba74d2bf1f53039ffe2ad2b0e1e251618575fc
Jeremiah King <usaxray25@gmail.com>
2022-12-14 19:20:45 -0600

    working on views

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    models.DateField(auto_now=False, auto_now_add=False)
+    publication_date = models.DateField(auto_now=False, auto_now_add=False)
     image_url = models.URLField(max_length=200)
     content = models.CharField(max_length=1000)
     objects = models.Manager()
---
+++ rareapi/views/category_view.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models.category import Category
+
+class CategoryView(ViewSet):
+    def retrieve(self, request, pk):
+        """Handle GET requests for category
+        """
+        try:
+            categories = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(categories)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """"Handle GET requests to handle all categories"""
+        categories = Category.objects.all()
+
+        category = request.query_params.get('type', None)
+        if category is not None:
+            category = categories.filter(category_id=category)
+
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, pk):
+        """Handle GET requests for category
+        """
+        try:
+            categories = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(categories)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a category
+
+        Returns:
+            Response -- Empty body with 204 status code
+            """
+
+        category = Category.objects.get(pk=pk)
+        category.label = request.data["label"]
+        category.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        """this deletes stuff
+        """
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
+        depth = 1
