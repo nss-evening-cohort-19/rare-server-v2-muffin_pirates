c02da58959b1605aa2274a99e998a4edf2557e83
Kristen Chandler <krismchandler@gmail.com>
2022-12-14 18:47:27 -0600

    push to review errors- do not approve

--- rareapi/models/comments.py
+++ rareapi/models/comments.py
@@ -4,7 +4,7 @@ from .post import Post
 
 
 class Comment(models.Model):
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     created_on = models.DateField(auto_now=False, auto_now_add=False)
