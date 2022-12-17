67f8705cf4629ee0c46f995a4b87da5eaa1217ab
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-12-17 13:38:59 -0600

    Views working after merge

--- rareapi/views/comments.py
+++ rareapi/views/comments.py
@@ -72,3 +72,9 @@ class CommentView(ViewSet):
     
     comment.save()
     return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+  def destroy(self, request, pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.delete()
+    
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
